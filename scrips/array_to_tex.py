a53e1230ef847c86a1d002d8cd837960871618e9
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2025-06-01 19:44:47 +0200

    fixed array_to_tex.py

--- scrips/array_to_tex.py
+++ scrips/array_to_tex.py
@@ -2,11 +2,10 @@ import numpy as np
 
 #Eingabe:
 d = np.loadtxt('csv_data.csv', skiprows=1, delimiter=',')
-d = d.transpose()
 quant_len = d.shape[0]
 list_len = d.shape[1]
+data = d.transpose()
 quantities_and_units = [['$T$', '$\\eta$', '$\\tau$'], ['°C', 'mPas', 'Pa']]
-data = np.array(d)
 err = np.array([[0.1 for _ in range(d.shape[1])], [0.1 for _ in range(d.shape[1])], [0.1 for _ in range(d.shape[1])]])
 caption = 'Änderung der Viskosität bei Temperaturerhöhung'
 label = 'Tabelle 1'
@@ -18,24 +17,30 @@ def find_row_len():
     for i in range(3,6):
         if list_len % i == 0:
             return i
-        len.append(i)
-    return max(len)
+        len.append(i - list_len % i)
+    return len.index(min(len)) + 3
 
 
 row_len = find_row_len()
 col_len = list_len // row_len * quant_len
+folds = list_len // row_len + (list_len % row_len > 0)
 
 s = '\\begin{table}[h] \n\\centering \n\\caption{' + caption + '}\n\\begin{tabular}{|c||'
 for i in range(row_len):
     s += 'c|'
-s += '}\n\\hline\n'
-for i in range(col_len):
-    n = i % quant_len
-    s += quantities_and_units[0][n] + '(' + quantities_and_units[1][n] + ') '
-    for j in range(row_len):
-        m = j + i // quant_len * quant_len
-        s += '& \\SI{' + str(data[n][m]) + ' \\pm ' + str(err[n][m]) + '}{} '
-    s += '\\\\\n\\hline\n'
+s += '}\n'
+
+for i in range(folds):
+    s += '\\hline\n\\'
+    for j in range(quant_len):
+        s += quantities_and_units[0][j] + '(' + quantities_and_units[1][j] + ') '
+        for k in range(row_len):
+            k = k + row_len * i
+            if k == list_len:
+                s += '&'
+                continue
+            s += '& \\SI{' + str(data[j][k]) + ' \\pm ' + str(err[j][k]) + '}{} '
+        s += '\\\\\n\\hline\n'
 s += '\\end{tabular}\\label{tab: ' + label + '}\n'
 s += '\\end{table}'
 
