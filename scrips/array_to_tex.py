b8b86b0272ec8becb239da654372acccb2c4b0ab
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2025-06-01 17:54:15 +0200

    array_to_tex.py checkpoint

--- scrips/array_to_tex.py
+++ scrips/array_to_tex.py
@@ -3,6 +3,8 @@ import numpy as np
 #Eingabe:
 d = np.loadtxt('csv_data.csv', skiprows=1, delimiter=',')
 d = d.transpose()
+quant_len = d.shape[0]
+list_len = d.shape[1]
 quantities_and_units = [['$T$', '$\\eta$', '$\\tau$'], ['°C', 'mPas', 'Pa']]
 data = np.array(d)
 err = np.array([[0.1 for _ in range(d.shape[1])], [0.1 for _ in range(d.shape[1])], [0.1 for _ in range(d.shape[1])]])
@@ -11,14 +13,28 @@ label = 'Tabelle 1'
 
 
 #Verarbeitung
+def find_row_len():
+    len = []
+    for i in range(3,6):
+        if list_len % i == 0:
+            return i
+        len.append(i)
+    return max(len)
+
+
+row_len = find_row_len()
+col_len = list_len // row_len * quant_len
+
 s = '\\begin{table}[h] \n\\centering \n\\caption{' + caption + '}\n\\begin{tabular}{|c||'
-for i in range(data.shape[1]):
+for i in range(row_len):
     s += 'c|'
 s += '}\n\\hline\n'
-for i in range(data.shape[0]):
-    s += quantities_and_units[0][i] + '(' + quantities_and_units[1][i] + ') '
-    for j in range(data.shape[1]):
-        s += '& \\SI{' + str(data[i][j]) + ' \\pm ' + str(err[i][j]) + '}{} '
+for i in range(col_len):
+    n = i % quant_len
+    s += quantities_and_units[0][n] + '(' + quantities_and_units[1][n] + ') '
+    for j in range(row_len):
+        m = j + i // quant_len * quant_len
+        s += '& \\SI{' + str(data[n][m]) + ' \\pm ' + str(err[n][m]) + '}{} '
     s += '\\\\\n\\hline\n'
 s += '\\end{tabular}\\label{tab: ' + label + '}\n'
 s += '\\end{table}'
