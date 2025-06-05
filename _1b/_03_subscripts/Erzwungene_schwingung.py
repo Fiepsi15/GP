eabe56bb3a2f1cc7fb8235a25a28af6345e4b9fa
LordZoro13 <foro7777777@gmail.com>
2025-06-05 19:34:18 +0200

    Anfang von phi-fit

--- _1b/_03_subscripts/Erzwungene_schwingung.py
+++ _1b/_03_subscripts/Erzwungene_schwingung.py
@@ -35,11 +35,13 @@ def alpha_fit(data, error):
     alpha_err = error[1]
 
     popt, pcov = curve_fit(model, omega, alpha, sigma=alpha_err, absolute_sigma=True)
-    dgamma = np.sqrt(pcov[0][0])
+    
+    F_m = popt[0]
+    omega0 = popt[1]
+    gamma = popt[2]
 
-
-    omega = np.linspace(0, np.max(omega),2000)
-    A = model(omega,popt[0],popt[1],popt[2])
+    omega = np.linspace(np.min(omega), np.max(omega),200)
+    A = model(omega,F_m,omega0,gamma)
 
 
     #bestimmung des maximums
@@ -48,21 +50,26 @@ def alpha_fit(data, error):
     omega_max = omega[max]
 
 
+
     #TODO: überarbeiten; funktioniert nicht
     A_halb = A_max/np.sqrt(2) # unsere Wertsbreite
-    halb = np.where(np.round(A,1) == np.round(A_halb,1))
-    A_halb = A[halb]
-    omega_halb = omega[halb]
-    #print("A_halb =" + A_halb)
-    
+    omega_halb = np.array([])
+    A_delta = A-A_halb
+    for i in range(len(A)-1):
+        if(A_delta[i]*A_delta[i+1]<0):
+            if(np.abs(A_delta[i])< np.abs(A_delta[i+1])):
+                omega_halb = np.append(omega_halb,omega[i])
+            else: omega_halb = np.append(omega_halb,omega[i+1])
 
     print(f"A_max = {A_max}, omega_max = {omega_max}")
+    print(f"A_halb = {A_halb}, omega_halb = {omega_halb}")
+    print(f"gamma = {(omega_halb[1]-omega_halb[0])/(2)}")
 
-
-    #plt.scatter(2,A_halb)
-    plt.scatter(omega_max, A_max, color='red', label = r"$A_\text{max}$", zorder=2)
-    #plt.scatter(omega_halb, A_halb, color='red', label = r"$\frac{1}{\sqrt{2}} \cdot A_\text{max}$", zorder = 2)
-    plt.plot(omega_halb,A_halb, color = 'red', label= r"Halbwertsbreite", zorder = 1)
+    
+    
+    plt.scatter(omega_max, A_max, color='red', label = r"$A_\text{max}$", zorder=4)
+    plt.scatter(omega_halb, [A_halb,A_halb], color='black', label = r"$\frac{1}{\sqrt{2}} \cdot A_\text{max}$", zorder = 3)
+    plt.plot(omega_halb,[A_halb,A_halb], color = 'black', label= r"$\Delta\omega$", zorder = 1)
     plt.plot(omega, A, label = r"Fit an Daten",  zorder = 0)
     
     plt.xlabel(r'$\omega$ (1/s)')
@@ -75,9 +82,23 @@ def alpha_fit(data, error):
     return
 
 
+def phi_fit(data,error):
+    def model(omega,omega0,gamma):
+        return(np.arctan((2*gamma*omega)/(omega0**2-omega**2)))
+    
+    omega = data[0]
+    phi = data[2]
+    omega_err = error[0]
+    phi_err = error[2]
+
+    popt, pcov = curve_fit(model, omega, phi, sigma=phi_err, absolute_sigma=True)
+
+    omega = np.linspace(np.min(omega), np.max(omega),200)
+    
+
 
 forced_300mA = np.loadtxt("_1b/_03_daten/Erzwungen_300_mA.csv", skiprows=1, delimiter=',').transpose()
-forced_300mA[1] = forced_300mA[1] / 1e-3  # mA to A
+forced_300mA[1] = forced_300mA[1] * 1e-3  # mA to A
 forced_300mA[0] = forced_300mA[0] * 2 * np.pi #F to omega
 err_f_300 = np.array([[0.001 for _ in range(forced_300mA.shape[1])],
                     [0.001 for _ in range(forced_300mA.shape[1])]])
