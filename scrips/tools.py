35e1c353e306836f4f1e1ed20db6e61487e77824
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2025-06-20 14:01:07 +0200

    finally did the scientific rounding should actually work :)

--- scrips/tools.py
+++ scrips/tools.py
@@ -7,7 +7,6 @@ def round_up(n, decimals=0):
     return np.round(n * 10 ** -decimals, decimals + 2)
 
 
-# TODO Make entire funktion for scientific rounding
 def round_to_any_digit(n, decimals=0):
     n = n * 10 ** decimals
     n = np.round(n, 0)
@@ -16,29 +15,26 @@ def round_to_any_digit(n, decimals=0):
 
 def find_first_nonzero_digit(n):
     n_s = str(n)
-    position = 0
     pre_decimals = 0
     for i in range(len(n_s)):
         if n_s[i] == '.':
             pre_decimals = i
+            break
+    position = 0
+    for i in range(len(n_s)):
+        if n_s[i] == '.':
             continue
         if n_s[i] != '0':
-            position = i if pre_decimals == 0 else i - 1
+            position = i if pre_decimals == 0 else i - pre_decimals
             break
     return pre_decimals, position
 
 
 def sci_round(n, delta_n, delta_delta_n=1/3):
-    delta_n_s = str(delta_n)
-    print(delta_n_s)
-    print(delta_n * delta_delta_n)
-    pre_decimals, position = find_first_nonzero_digit(delta_n * delta_delta_n)
+    dpre_decimals, dposition = find_first_nonzero_digit(delta_n * delta_delta_n)
 
-    print(position, pre_decimals)
-    try:
-        print(delta_n_s[position + pre_decimals])
-    except IndexError:
-        print("Uncertainty * Uncertainty**2 is to close to one, floating point math. ..")
-        return
+    if dposition < 0:
+        dposition += 1
+    return round_to_any_digit(n, dposition), round_up(delta_n, dposition)
 
 
