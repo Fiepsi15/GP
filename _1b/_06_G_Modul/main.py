865fab6dd6146cc54038fe86e0539c8387b1f25c
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2025-07-20 19:34:11 +0200

    fixed the missing diameter to radius conversion

--- _1b/_06_G_Modul/main.py
+++ _1b/_06_G_Modul/main.py
@@ -2,7 +2,6 @@ import numpy as np
 from _1b._06_G_Modul.subscripts import GReg as sub
 from _1b._06_G_Modul.subscripts import Moment_of_Inertia as MoI
 
-#Todo: make the plots
 
 def unwrap_and_mean(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     data_bar = np.mean(data, axis=1)
@@ -18,6 +17,8 @@ dr = 0.01e-3 / 2 # Uncertainty in radius of the Wire(m)
 # Calculate the moments of inertia for the rings
 rings = np.loadtxt('_1b/_06_G_Modul/daten/ring_data.csv', skiprows=1, delimiter=',').transpose()[1:4, :] * 1e-3  # Load data and convert to kg and meters
 print(rings)
+rings[1] = rings[1] / 2  # Convert diameter to radius
+rings[2] = rings[2] / 2  # Convert diameter to radius
 moments_of_inertia, Delta_moments_of_inertia = MoI.get_moment_of_inertia(rings[0], dm, rings[1], dR, rings[2], dR)
 print(moments_of_inertia, Delta_moments_of_inertia)
 
