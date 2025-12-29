e9b079ab34563920de371b99798ddde299d5bf53
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2025-12-29 15:32:19 +0100

    feat: enhance array_to_tex integration and update tau calculations across multiple scripts

--- _1c/_24_Wirbelstrom/subscripts/geschwindigkeit.py
+++ _1c/_24_Wirbelstrom/subscripts/geschwindigkeit.py
@@ -1,11 +1,15 @@
 import numpy as np
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
+from scrips.array_to_tex import array_to_tex as a2t
 
 
 def geschwindigkeit(t: np.ndarray, a: np.ndarray):
     def model(sina, f, b):
         return sina * f + b
+
+    alpha = np.arctan(a / 50) * 360 / (2 * np.pi)
+    alpha_err = 1 / (1 + (a / 50) ** 2) * (1 / 50) * 360 / (2 * np.pi) * 0.1
 
     s = 0.1
     s_err = 1e-3
@@ -15,6 +19,10 @@ def geschwindigkeit(t: np.ndarray, a: np.ndarray):
         v[i] = s / np.mean(t[i])
         v_err[i] = np.sqrt((s / (v[i] ** 2) * np.std(t[i])) ** 2 + (s_err / v[i]) ** 2)
 
+    werte = np.array([alpha, v])
+    werte_err = np.array([alpha_err, v_err])
+    a2t(werte, werte_err, [['$\\alpha$', '$v_\\infty$'], ['°', '$\\frac{\\mathrm{m}}{\\mathrm{s}}$']],
+        'Mittelwerte der Endgeschwindigkeiten bei Variation des Neigungswinkels', 'neigung_geschwindigkeit')
 
     x = np.sin(np.arctan(a / 50))
     popt, pcov = curve_fit(model, xdata=x, ydata=v, sigma=v_err, absolute_sigma=True)
