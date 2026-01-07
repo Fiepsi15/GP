b6afeb2edac654bc5ea45f7752ebf12fec074385
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-01-07 19:48:11 +0100

    feat: add row swapping function and enhance data processing for Versuch2 and Versuch3

--- _1c/_28_Schwingkreis/main.py
+++ _1c/_28_Schwingkreis/main.py
@@ -9,15 +9,27 @@ a, b, delta_a, delta_b = v1.linear_regression(Versuch1_daten[1], Versuch1_daten[
 v1.logarithmic_plot(Versuch1_daten[1], Versuch1_daten[0], 1.96e-3, a, b, delta_a, delta_b)
 Lambda, delta_Lambda = v1.logarithmic_decrement(Versuch1_daten[1], Versuch1_daten[0], 1.96e-3)
 
+def swap_rows(matrix, col1, col2):
+    h = np.copy(matrix[col1])
+    matrix[col1] = matrix[col2]
+    matrix[col2] = h
+    return matrix
+
 niederfrequenz_daten = np.loadtxt('_1c/_28_Schwingkreis/daten/niederfrequenzbereich.csv', skiprows=1, delimiter=',').transpose()
 grenz_daten = np.loadtxt('_1c/_28_Schwingkreis/daten/Grenzbereich.csv', skiprows=1, delimiter=',').transpose()
 hochfrequenz_daten = np.loadtxt('_1c/_28_Schwingkreis/daten/hochfrequenzbereich.csv', skiprows=1, delimiter=',').transpose()
+hochfrequenz_daten[3] = hochfrequenz_daten[3] - 180
 hochfrequenz_daten[0] = hochfrequenz_daten[0] * 1e3  # Umrechnung kHz in Hz
 
-Versuch2_daten = np.concatenate((niederfrequenz_daten, grenz_daten), axis=1)
+niederfrequenz_daten = swap_rows(niederfrequenz_daten, 1, 2)
+grenz_daten = swap_rows(grenz_daten, 1, 2)
+
+Versuch2_daten = np.concatenate((niederfrequenz_daten, grenz_daten, hochfrequenz_daten), axis=1)
 v2.gesamtbereich(Versuch2_daten[0], Versuch2_daten[1], Versuch2_daten[2])
 
-v2.phasenverschiebung(grenz_daten[0], grenz_daten[3])
+omega_0, delta_omega_0 = v2.phasenverschiebung(Versuch2_daten[0], Versuch2_daten[3])
+
+R, delta_R = v2.widerstand(grenz_daten[0], grenz_daten[1], grenz_daten[2], omega_0, delta_omega_0)
 
 Versuch3_daten = np.loadtxt('_1c/_28_Schwingkreis/daten/Versuch3.csv', skiprows=1, delimiter=',').transpose()
 
--- _1c/_28_Schwingkreis/subscripts/Versuch2.py
+++ _1c/_28_Schwingkreis/subscripts/Versuch2.py
@@ -6,21 +6,60 @@ from scrips.tools import sci_round
 
 def gesamtbereich(omega, U_r, U):
     R_v = 82
+    I = U_r / R_v
     omega_0_r = 3070
     delta_omega_0_r = 200
-    Z = U * R_v / U_r
+    Z = U / I
 
-    plt.errorbar(omega, Z, label='Messwerte', fmt='o')
-    plt.plot([omega_0_r, omega_0_r], [0, 50], label=f'$\\omega_0 = {omega_0_r} \\pm {delta_omega_0_r}$', color='green')
+    plt.errorbar(omega, Z, label='Messwerte', fmt='o', color='blue')
+    plt.plot([omega_0_r, omega_0_r], [0, 0.1], label=f'$\\omega_0 = {omega_0_r} \\pm {delta_omega_0_r}$', color='green')
+    plt.plot([omega_0_r - delta_omega_0_r, omega_0_r - delta_omega_0_r], [0, 0.1], color='green', linestyle='--')
+    plt.plot([omega_0_r + delta_omega_0_r, omega_0_r + delta_omega_0_r], [0, 0.1], color='green', linestyle='--')
+    plt.xlabel('Frequenz $\\omega (\\mathrm{Hz})$')
+    plt.ylabel('$Z \\;(\\Omega)$')
+    plt.legend()
     plt.show()
+
+
+def widerstand(omega, U_r, U, omega_0, delta_omega_0):
+    R_v = 82
+    I = U_r / R_v
+    Z = U / I
+    omega_0_r, delta_omega_0_r = sci_round(omega_0, delta_omega_0)
+    def model(omega, a, b, c):
+        return a * omega ** 2 + b * omega + c
+
+    grenzomega = np.array([omega[i] for i in range(3, 8)])
+    grenzZ = np.array([Z[i] for i in range(3, 8)])
+    popt, pcov = opt.curve_fit(model, grenzomega, grenzZ)
+    Z_0 = model(omega_0, *popt)
+    Z_0p = model(omega_0 + delta_omega_0, *popt)
+    Z_0m = model(omega_0 - delta_omega_0, *popt)
+    delta_Z_0 = np.abs((Z_0p - Z_0m) / 2)
+    Z_0r, delta_Z_0r = sci_round(Z_0, delta_Z_0)
+    print(Z_0r, delta_Z_0r)
+
+
+    plt.errorbar(omega, Z, label='Messwerte', fmt='o', color='blue')
+    x = np.linspace(grenzomega[0], grenzomega[-1], 100)
+    plt.plot(x, model(x, *popt), color='red', label='quadratische näherung um $\\omega_0$')
+    plt.plot([omega_0, omega_0], [100, 200], label=f'$\\omega_0 = {omega_0_r} \\pm {delta_omega_0_r}$', color='green')
+    plt.plot([omega_0 - delta_omega_0, omega_0 - delta_omega_0], [100, 200], color='green', linestyle='--')
+    plt.plot([omega_0 + delta_omega_0, omega_0 + delta_omega_0], [100, 200], color='green', linestyle='--')
+    plt.xlabel('Frequenz $\\omega (\\mathrm{Hz})$')
+    plt.ylabel('$Z \\;(\\Omega)$')
+    plt.legend()
+    plt.grid()
+    plt.show()
+    return Z_0, delta_Z_0
 
 
 def phasenverschiebung(omega, phi):
     def model(omega, a, b):
         return omega * a + b
 
-    grenzomega = np.array([omega[i] for i in range(4, 7)])
-    grenzphi = np.array([phi[i] for i in range(4, 7)])
+    grenzomega = np.array([omega[i] for i in range(16, 19)])
+    grenzphi = np.array([phi[i] for i in range(16, 19)])
 
     popt, pcov = opt.curve_fit(model, grenzomega, grenzphi, sigma=[0.5 for _ in grenzphi], absolute_sigma=True)
     a, b = popt
@@ -42,6 +81,9 @@ def phasenverschiebung(omega, phi):
     plt.plot([omega_0, omega_0], [-20, 20], label=f'$\\omega_0 = {omega_0_r} \\pm {delta_omega_0_r}$', color='green')
     plt.plot([omega_0 - delta_omega_0, omega_0 - delta_omega_0], [-20, 20], linestyle='dashed', color='green')
     plt.plot([omega_0 + delta_omega_0, omega_0 + delta_omega_0], [-20, 20], linestyle='dashed', color='green')
+    plt.xscale('log')
     plt.legend()
     plt.grid(True)
     plt.show()
+
+    return omega_0, delta_omega_0
