4b76c31a4adcd490be8fc26406c0e68074b63877
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-01-11 19:10:49 +0100

    Add I_BE parameter to plots and implement signal_boost_worse function

--- _1c/_26_Kennlinien/subscripts/transistor.py
+++ _1c/_26_Kennlinien/subscripts/transistor.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scrips.tools import sci_round
+from scrips.array_to_tex import array_to_tex as a2t
 
 
-def plots(T, T_err, __color):
+def plots(T, T_err, __color, I_BE):
     U_CE, I_CE = T
     U_CE_err, I_CE_err = T_err
-    plt.errorbar(U_CE, I_CE, xerr=U_CE_err, yerr=I_CE_err, label='Transistor Kennlinie', color=__color, fmt='o',
+    plt.errorbar(U_CE, I_CE, xerr=U_CE_err, yerr=I_CE_err, label='$I_{BE} =$' + str(I_BE) + 'mA', color=__color,
+                 fmt='o',
                  capsize=5)
     return
 
@@ -37,29 +39,39 @@ def signal_boost(I_BE, I_CE):
     return sci_round(beta, beta_err)
 
 
+def signal_boost_worse(I_BE, I_BE_err, I_CE, I_CE_err):
+    B = I_CE / I_BE
+    B_err = np.sqrt((I_CE_err / I_BE) ** 2 + (I_CE * I_BE_err / I_BE ** 2) ** 2)
+    a2t(np.array([I_BE, B]), np.array([I_BE_err, B_err]), [['I', 'B'], ['A','']], 'Die Großsignalverstärkung $B$ In Abhängigkeit des Basisstroms $I_\\mathrm{BE}$.', 'Großsignalverstärkung')
+    return
+
+
+
 def transistor(T10, T10_err, T20, T20_err, T30, T30_err, T39, T39_err):
     I_BE = np.array([10, 20, 30, 39]) * 1e-6  # in Amperes
     I_CE = np.array([T10[1][6], T20[1][6], T30[1][6], T39[1][6]]) * 1e-3  # in Amperes
+    I_CE_err = np.array([T10_err[1][6], T20_err[1][6], T30_err[1][6], T39_err[1][6]]) * 1e-3  # in Amperes
     beta, beta_err = signal_boost(I_BE, I_CE)
     print(f'Stromverstärkung: β = {beta} ± {beta_err}')
+    signal_boost_worse(I_BE, np.full_like(I_BE, 0.1 * 1e-6), I_CE, I_CE_err)
 
-    colors = ["#4BB6A7", "#4D96C2", "#5578D1", "#6A5DCB", "#8B56B1"]
+    colors = ["#4BB6A7", "#4D96C2", "#5578D1", "#6A5DCB"]
     r, r_err = linreg(T10, 3)
     print(f'Ausgangswiderstand bei 10: r_CE = {r} ± {r_err} Ohm')
-    plots(T10, T10_err, colors[0])
+    plots(T10, T10_err, colors[0], 10)
     r, r_err = linreg(T20, 3)
     print(f'Ausgangswiderstand bei 20: r_CE = {r} ± {r_err} Ohm')
-    plots(T20, T20_err, colors[1])
+    plots(T20, T20_err, colors[1], 20)
     r, r_err = linreg(T30, 3)
     print(f'Ausgangswiderstand bei 30: r_CE = {r} ± {r_err} Ohm')
-    plots(T30, T30_err, colors[2])
+    plots(T30, T30_err, colors[2], 30)
     r, r_err = linreg(T39, 3)
     print(f'Ausgangswiderstand bei 39: r_CE = {r} ± {r_err} Ohm')
-    plots(T39, T39_err, colors[3])
+    plots(T39, T39_err, colors[3], 39)
     plt.xlabel('$U_{CE} (V)$')
     plt.ylabel('$I_{CE} (mA)$')
     plt.minorticks_on()
     plt.tick_params(direction='in', which='both')
-    plt.grid(True)
-    # plt.legend()
+    plt.grid()
+    plt.legend()
     plt.show()
