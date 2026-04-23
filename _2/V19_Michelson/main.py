edaeb16f6396d16eff8eb2dfe32bb6de00476abe
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-04-23 15:42:51 +0200

    replaced through std with measurement uncertainty

--- _2/V19_Michelson/main.py
+++ _2/V19_Michelson/main.py
@@ -19,21 +19,28 @@ def get_lambda(kappa, delta_x, N, delta_kappa=0, delta_delta_x=0, delta_N=0):
 
 
 delta_x_meas = np.array([[0.8, 0.83], [0.82, 0.83], [0.84, 0.86]]) / 1e3
+delta_delta_x = np.array([np.sqrt(2) * 10 for _ in range(len(delta_x_meas))]) / 1e6
 N = 200
+delta_N = 10
 lambda_Laser = 532e-9
 delta_x = np.mean(delta_x_meas, axis=1)
 delta_x_m = np.mean(delta_x)
-delta_delta_x = np.std(delta_x) / np.sqrt(len(delta_x))
 
-kappa, delta_kappa = get_kappa(np.mean(delta_x_m), lambda_Laser, N, delta_lambda=1e-9, delta_delta_x=delta_delta_x, delta_N=10)
+delta_delta_x = delta_delta_x[0] / np.sqrt(len(delta_x))
+
+kappa, delta_kappa = get_kappa(np.mean(delta_x_m), lambda_Laser, N, delta_lambda=1e-9, delta_delta_x=delta_delta_x, delta_N=delta_N)
 kappa_r, delta_kappa_r = sci_round(kappa, delta_kappa)
 
+print(kappa, delta_kappa)
 print(kappa_r, delta_kappa_r)
 
 delta_x_unknown = np.array([[0.96], [0.99], [0.97]]) / 1e3
+delta_delta_x = np.array([50 for _ in range(len(delta_x_meas))]) / 1e6
 delta_x_um = np.mean(delta_x_unknown)
-delta_delta_x_u = np.std(delta_x_unknown) / np.sqrt(len(delta_x_unknown))
-lambda_unknown, delta_lambda = get_lambda(kappa, np.mean(delta_x_unknown), N, delta_kappa=delta_kappa, delta_delta_x=delta_delta_x_u, delta_N=10)
+delta_delta_x_u = delta_delta_x[0] / np.sqrt(len(delta_x_unknown))
+
+lambda_unknown, delta_lambda = get_lambda(kappa, np.mean(delta_x_unknown), N, delta_kappa=delta_kappa, delta_delta_x=delta_delta_x_u, delta_N=delta_N)
 lambda_r, delta_lambda_r = sci_round(lambda_unknown, delta_lambda)
 
+print(lambda_unknown, delta_lambda)
 print(lambda_r, delta_lambda_r)
