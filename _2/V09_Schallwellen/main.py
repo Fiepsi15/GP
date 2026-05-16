1bdf0a2f143112f55bb4dfe1c4181eeb01b69097
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-05-16 18:11:39 +0200

    improved graphing

--- _2/V09_Schallwellen/main.py
+++ _2/V09_Schallwellen/main.py
@@ -1,34 +1,37 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import _2.V09_Schallwellen.subscripts.festkörper as fk
+#import _2.V09_Schallwellen.subscripts.festkörper as fk
 import _2.V09_Schallwellen.subscripts.gase as gase
 
-def gasprep(data):
+def gas_prep(data):
     nu = data[0] * 1e3
-    dist = np.abs(data[1] - data[2]) * 1e-3
+    d = np.abs(data[1] - data[2]) * 1e-3
     N = data[3]
 
     d_nu = 1
-    d_dist = np.sqrt(2) * 1e-3
+    d_d = np.sqrt(2) * 1e-3
     d_N = 1
-    return nu, dist, N, d_nu, d_dist, d_N
+    return nu, d, N, d_nu, d_d, d_N
 
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), gridspec_kw={'wspace': 0.3})
+fig.suptitle('Schallgeschwindigkeit in')
 
 # Schallgeschwindigkeit in Luft:
 
 luft_daten = np.loadtxt('_2/V09_Schallwellen/data/Luft.csv', skiprows=1, delimiter=",").transpose()
-nu, dist, N, d_nu, d_dist, d_N = gasprep(luft_daten)
+freq, distanz, Anz, d_freq, d_distanz, d_Anz = gas_prep(luft_daten)
 
-_ = gase.get_sonic_speed_reg(nu, dist, N, ax1, d_nu, d_dist, d_N)
+_ = gase.get_sonic_speed_reg(freq, distanz, Anz, ax1, d_freq, d_distanz, d_Anz, plot_title='Luft')
 
 
 # Schallgeschwindigkeit in CO2:
 
 luft_daten = np.loadtxt('_2/V09_Schallwellen/data/CO2.csv', skiprows=1, delimiter=",").transpose()
-nu, dist, N, d_nu, d_dist, d_N = gasprep(luft_daten)
+freq, distanz, Anz, d_freq, d_distanz, d_Anz = gas_prep(luft_daten)
 
-_ = gase.get_sonic_speed_reg(nu, dist, N, ax2, d_nu, d_dist, d_N)
+_ = gase.get_sonic_speed_reg(freq, distanz, Anz, ax2, d_freq, d_distanz, d_Anz, plot_title='CO2')
+
+print(2 * distanz[-1] * freq[-1] / Anz[-1])
 
 plt.show()
--- _2/V09_Schallwellen/subscripts/gase.py
+++ _2/V09_Schallwellen/subscripts/gase.py
@@ -4,23 +4,26 @@ from scipy.optimize import curve_fit
 from scrips.tools import sci_round
 
 
-def get_sonic_speed_reg(nu, d, N, plot_axis, d_nu, d_d, d_N):
+def get_sonic_speed_reg(nu, d, N, plot_axis, d_nu, d_d, d_N, plot_title=''):
     def  model(x, c):
         return x * c
 
-    x = N
-    y = 2 * d * nu
-    y_err = np.sqrt((2 * nu * d_d) ** 2
-                 + (2 * d * d_nu) ** 2)
+    x = N / (2 * d)
+    y = nu
+    y_err = d_nu
+    x_err = np.sqrt((d_N / (2 * d)) ** 2
+                    + (N / (2 * d ** 2) * d_d) ** 2)
+
     popt, pcov = curve_fit(model, x, y, sigma=y_err, absolute_sigma=True)
     c = popt[0]
     d_c = np.sqrt(np.diag(pcov))
     c_r, dc_r = sci_round(c, d_c)
 
-    plot_axis.errorbar(x, y, yerr=y_err, xerr=np.full_like(y_err, d_N), fmt='.', capsize=5, color='blue', label='Messdaten')
+    plot_axis.errorbar(x, y, yerr=y_err, xerr=x_err, fmt='.', capsize=5, color='blue', label='Messdaten')
     plot_axis.plot(x, model(x, c), color='red', label=f'Fit: $c = {c_r} \\pm {dc_r}$')
-    plot_axis.set_xlabel('N')
-    plot_axis.set_ylabel('$2 d \\nu\\;/\\;\\mathrm{m/s}$ ')
+    plot_axis.set_title(plot_title)
+    plot_axis.set_xlabel('$\\frac{N}{2d}/(\\mathrm{m})^{-1}$')
+    plot_axis.set_ylabel('$\\nu/\\mathrm{Hz}$ ')
     plot_axis.grid(True)
     plot_axis.legend()
 
