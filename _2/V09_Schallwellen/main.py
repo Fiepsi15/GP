871b5703ea64a1bca7c4d44df3991121442a8236
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-05-21 21:46:37 +0200

    Calculations Done

--- _2/V09_Schallwellen/main.py
+++ _2/V09_Schallwellen/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import _2.V09_Schallwellen.subscripts.festkörper as fk
 import _2.V09_Schallwellen.subscripts.gase as gase
+from scrips.tools import sci_round
 
 
 def gas_prep(data):
@@ -11,7 +12,7 @@ def gas_prep(data):
 
     d_nu = 1
     d_d = np.sqrt(2) * 1e-3
-    d_N = 1
+    d_N = 0
     return nu, d, N, d_nu, d_d, d_N
 
 
@@ -22,6 +23,7 @@ def rule1(i):
 def rule2(i):
     return 2 * i + 1
 
+_, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), gridspec_kw={'wspace': 0.3})
 
 frequency_1 = np.array(
     [[2.150, 6], [4.300, 6], [6.450, 6], [8.600, 5], [9.950, 2], [10.750, 5], [12.9, 2], [15.1, 3], [17.2, 2],
@@ -29,9 +31,19 @@ frequency_1 = np.array(
 
 frequency_2 = np.array([4.3, 8.6, 12.9, 17.2, 15.6, 19.9, 21.5]) * 1e3
 
-fk.rod(frequency_1[0], rule1)
+c1, dc1 = fk.rod(frequency_1[0], rule1, ax1, 'Einspannung bei $1/2$ der Länge')
 print('\n Second arrangement:')
-s = fk.rod(frequency_2, rule2)
+c2, dc2 = fk.rod(frequency_2, rule2, ax2, 'Einspannung bei $1/4$ und $3/4$ der Länge')
+plt.show()
+
+c1/2 + c2/2
+
+c_S = np.mean([c1, c2])
+dc_S = np.sqrt((dc1 / 2) ** 2 + (dc2 / 2) ** 2)
+c_Sr, dc_Sr = sci_round(c_S, dc_S)
+print(f'Metall: c = {c_Sr} pm {dc_Sr}')
+
+# Gase:
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), gridspec_kw={'wspace': 0.3})
 fig.suptitle('Schallgeschwindigkeit in')
@@ -45,11 +57,16 @@ _ = gase.get_sonic_speed_reg(freq, distanz, Anz, ax1, d_freq, d_distanz, d_Anz, 
 
 # Schallgeschwindigkeit in CO2:
 
-luft_daten = np.loadtxt('_2/V09_Schallwellen/data/CO2.csv', skiprows=1, delimiter=",").transpose()
-freq, distanz, Anz, d_freq, d_distanz, d_Anz = gas_prep(luft_daten)
+CO2_daten = np.loadtxt('_2/V09_Schallwellen/data/CO2.csv', skiprows=1, delimiter=",").transpose()
+freq, distanz, Anz, d_freq, d_distanz, d_Anz = gas_prep(CO2_daten[:,0:-1])
 
 _ = gase.get_sonic_speed_reg(freq, distanz, Anz, ax2, d_freq, d_distanz, d_Anz, plot_title='CO2')
 
-print(2 * distanz[-1] * freq[-1] / Anz[-1])
-
+freq, distanz, Anz, d_freq, d_distanz, d_Anz = gas_prep(CO2_daten[:,-1])
+c_CO2_true = 2 * distanz * freq / Anz
+dc_CO2_true = np.sqrt((2 * freq / Anz * d_distanz) ** 2
+                     + (2 * distanz / Anz * d_freq) ** 2
+                     + (2 * distanz * freq / Anz ** 2 * d_Anz) ** 2)
+c, dc = sci_round(c_CO2_true, dc_CO2_true)
+print(c, dc)
 plt.show()
--- _2/V09_Schallwellen/subscripts/festkörper.py
+++ _2/V09_Schallwellen/subscripts/festkörper.py
@@ -1,45 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from scipy.optimize import curve_fit
 from scrips.tools import sci_round
+from scrips.array_to_tex import array_to_tex as a2t
 
 
-def rod(frequencies, rule):
+def rod(frequencies, rule, ax, title):
     def model(x, c):
         return c * x
 
     l = 1.138
-
+    dl = 1e-3
+    c, dc = 0, 0
 
     selected = find_valid_frequencies(frequencies, rule=rule)
 
     for i in range(len(selected)):
         fset = np.array(selected[i]).transpose()
         x = rule(fset[0]) / l
+        x_err = rule(fset[0]) / l ** 2 * dl
         y = fset[1]
-        y_err = np.full_like(y, 50)
         y_err = np.array([(50 if (y[i] < 11e3) else 100) for i in range(len(y))])
         popt, pcov = curve_fit(model, x, y, sigma=y_err, absolute_sigma=True)
         c_val = popt[0]
         delta_c_val = np.sqrt(np.diag(pcov))[0]
         c_r, dc_r = sci_round(c_val, delta_c_val)
 
-        print(f'Value for c = {c_r} pm {dc_r}, with {len(selected[i])} frequencies and lowest order {fset[0][0]} at {fset[1][0]} Hz')
+        print(
+            f'Value for c = {c_r} pm {dc_r}, with {len(selected[i])} frequencies and lowest order {fset[0][0]} at {fset[1][0]} Hz')
 
         if i != 2 and frequencies[0] != 4.3e3:
             continue
-        print(f'Serie:\n {fset.transpose()}')
 
-        fit, ax = plt.subplots()
-        ax.scatter(x, y, label='Messdaten', color='blue')
-        ax.plot(x, model(x, c_val), label=f'Fit: $c = {c_r} \\pm {dc_r}$', color='red')
-        ax.set_xlabel('$\\frac{n + 1/2}{L} / \\mathrm{m}^{-1}$')
+        #a2t(np.array([x, y]), np.array([x_err, y_err]), [['$(2n + 1)/l$', '$\\nu$'], ['$\\mathrm{m}^{-1}$', '$\\mathrm{Hz}$']], override_row_len=3)
+        c, dc = c_val, delta_c_val
+
+        ax.grid()
+        ax.yaxis.minorticks_on()
+        ax.tick_params(direction='in', which='both')
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.errorbar(fset[0], y, yerr=y_err, label='Messdaten', color='blue', capsize=5, fmt='.')
+        ax.plot(fset[0], model(x, c_val), label=f'lineare Regression liefert:\n$c = ({c_r} \\pm {dc_r})\\; \\mathrm{'{'}m{'}'}/ \\mathrm{'{'}s{'}'}$', color='red')
+        ax.plot(fset[0], model(x, c_val + delta_c_val), label=f'$\\pm\\delta c$', color='grey', linestyle='dashed')
+        ax.plot(fset[0], model(x, c_val - delta_c_val), color='grey', linestyle='dashed')
+        ax.set_xlabel('$n$')
         ax.set_ylabel('$\\nu / \\mathrm{Hz}$')
         ax.legend()
-        ax.grid()
+        ax.set_title(title)
 
-    plt.show()
-    return selected
+    return c, dc
 
 
 def find_valid_frequencies(frequencies, rule):
