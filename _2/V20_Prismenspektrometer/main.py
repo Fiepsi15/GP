7390c685098cecbcf9a500a78bf46dec60c20c74
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-06-25 20:38:42 +0200

    did the main

--- _2/V20_Prismenspektrometer/main.py
+++ _2/V20_Prismenspektrometer/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from _2.V20_Prismenspektrometer.subscripts import prism_calculation as prism
+from scrips.tools import sci_round
 
 
 def decimal_deg(deg, min, delta_min):
@@ -18,7 +19,21 @@ def Hg_prism(dir, i):
     delta_min = decimal_deg(prism1[2], prism1[3], np.full_like(wavelength,
                                                                delta_angle))  # (grad, minuten, unsicherheit in min) → (grad, unsicherheit in grad)
 
-    return prism.get_n_of_lambda(wavelength, delta_min)
+    return prism.get_lambda_of_n(wavelength, delta_min)
+
+def make_spectrum(dir, lambda_of_n, lamp, prism_num, hg=False):
+    data = np.loadtxt(dir + f'{lamp}_prism{prism_num}.csv', delimiter=',', skiprows=1).transpose()
+
+    delta_angle = 10
+    delta_min = decimal_deg(data[1], data[2], np.full_like(data[1], delta_angle))
+    if hg:
+        delta_min = decimal_deg(data[2], data[3], np.full_like(data[1],
+                                                               delta_angle))  # (grad, minuten, unsicherheit in min) → (grad, unsicherheit in grad)
+    print(f'\nLampe: {lamp}')
+    if lamp == 'I':
+        lamp = '?'
+    prism.get_spectrum_of_minimum_deflection(lambda_of_n, delta_min, lamp=lamp)
+    return
 
 
 data_dir = '_2/V20_Prismenspektrometer/daten/'
@@ -27,8 +42,15 @@ data_dir = '_2/V20_Prismenspektrometer/daten/'
 _ = Hg_prism(data_dir, 1)
 
 # Prisma 2
-n_of_lambda = Hg_prism(data_dir, 3)
+n_of_lambda, lambda_of_n = Hg_prism(data_dir, 3)
 
+# Small sanity test:
+#l, dl = lambda_of_n((1.56, 0.002))
+#l, dl = sci_round(l, dl)
+#print(f'Test: λ(1.560(2)) = {l} ± {dl} nm')
 
+make_spectrum(data_dir, lambda_of_n, 'Hg', 3, hg=True)
+make_spectrum(data_dir, lambda_of_n, 'He', 3)
+make_spectrum(data_dir, lambda_of_n, 'I', 3)
 
 plt.show()
--- _2/V20_Prismenspektrometer/subscripts/prism_calculation.py
+++ _2/V20_Prismenspektrometer/subscripts/prism_calculation.py
@@ -1,18 +1,66 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import optimize
+from scrips.tools import sci_round
+
+
+def wavelength_to_rgb(wavelength, gamma=0.8):
+    '''This converts a given wavelength of light to an
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    '''
+
+    wavelength = float(wavelength)
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G = 0.0
+        B = (1.0 * attenuation) ** gamma
+    elif wavelength >= 440 and wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+    elif wavelength >= 490 and wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+    elif wavelength >= 580 and wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    #R *= 255
+    #G *= 255
+    #B *= 255
+    return (R, G, B)
 
 
 def calculate_n(delta_min, epsilon=60):
     epsilon = np.deg2rad(epsilon)
     delta_min = np.deg2rad(delta_min)
 
-    n = np.sin((delta_min[0] + epsilon)/2) / np.sin(epsilon / 2)
-    delta_n = np.abs(np.cos((delta_min[0] + epsilon)/2) / np.sin(epsilon / 2) * 1/2 * delta_min[1])
+    n = np.sin((delta_min[0] + epsilon) / 2) / np.sin(epsilon / 2)
+    delta_n = np.abs(np.cos((delta_min[0] + epsilon) / 2) / np.sin(epsilon / 2) * 1 / 2 * delta_min[1])
 
     return n, delta_n
 
-def get_n_of_lambda(wavelength, delta_min, epsilon = 60):
+
+def get_lambda_of_n(wavelength, delta_min, epsilon=60):
     def model(x, a, b):
         return a + b / x ** 2
 
@@ -25,6 +73,10 @@ def get_n_of_lambda(wavelength, delta_min, epsilon = 60):
         delta_n = np.sqrt((a[1] ** 2) + (b[1] / wavelength ** 2) ** 2)
         return n, delta_n
 
+    def lambda_of_n(n):
+        wavelength = np.sqrt(b[0] / (n[0] - a[0]))
+        delta_wavelength = np.abs(1 / (2 * wavelength) * b[0] / (n[0] - a[0]) ** 2 * n[1])
+        return wavelength, delta_wavelength
 
     x = np.linspace(wavelength[0], wavelength[-1], 100)
     fig, ax = plt.subplots()
@@ -38,10 +90,35 @@ def get_n_of_lambda(wavelength, delta_min, epsilon = 60):
     ax.errorbar(wavelength, n[0], yerr=n[1], label='Messwerte $n_{\\lambda}$', color='blue', fmt='o', capsize=2)
     ax.plot(x, n_of(x)[0], label='Ausgleichskurve $n(\\lambda)$', color='red')
     ax.fill_between(x, n_of(x)[0] + n_of(x)[1], n_of(x)[0] - n_of(x)[1], label='Unsicherheit', color='red', alpha=0.2)
-    ax.set(xlabel='Wellenlänge $\\lambda\\,[\\mathrm{nm}]$', ylabel='Brechungsindex $n\\,(\\lambda)$', title='Brechungsindex in Abhängigkeit der Wellenlänge')
+    ax.set(xlabel='Wellenlänge $\\lambda\\,[\\mathrm{nm}]$', ylabel='Brechungsindex $n\\,(\\lambda)$',
+           title='Brechungsindex in Abhängigkeit der Wellenlänge')
     ax.tick_params(axis='both', which='both', direction='in')
     ax.minorticks_on()
     ax.legend()
     ax.grid()
 
-    return n_of
+    return n_of, lambda_of_n
+
+
+def get_spectrum_of_minimum_deflection(lambda_of_n, delta_min, epsilon=60, lamp = ''):
+    n = calculate_n(delta_min, epsilon)
+    wavelength = lambda_of_n(n)
+
+    print('Spektrallinien:')
+
+    fig, ax = plt.subplots(figsize=(6, 1.5))
+    fig.subplots_adjust(bottom=0.3, top=0.82)
+
+    for wavelength, delta_wavelength in np.array(wavelength).transpose():
+        ax.plot([wavelength, wavelength], [0, 1], color=wavelength_to_rgb(wavelength)) #plt.get_cmap('Spectral')(1 - (wavelength - 400) / (700 - 400)))
+        ax.fill_betweenx([0, 1], [wavelength-delta_wavelength, wavelength-delta_wavelength], [wavelength+delta_wavelength, wavelength+delta_wavelength], color=wavelength_to_rgb(wavelength), alpha=0.2)
+        w_r, dw_r = sci_round(wavelength, delta_wavelength)
+        print(f'lambda = {w_r} +- {dw_r}')
+
+    ax.plot([], [], label='Spektrallinien', color='orange')
+    ax.fill_between([], [], [], label='Unsicherheit', color='orange', alpha=0.2)
+    ax.set(xlabel='Wellenlänge $\\lambda\\,[\\mathrm{nm}]$', ylabel=lamp, title='Spektrum', xlim=(380, 750), ylim=(0, 1))
+    ax.tick_params(axis='x', which='both', direction='in')
+    ax.tick_params(axis='y', which='both', left=False, labelleft=False)
+    ax.minorticks_on()
+    ax.legend(loc='upper right')
