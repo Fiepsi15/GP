689126e35ab6c69bb550d7e91952a84c8dc3ac39
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-07-17 19:46:44 +0200

    calculated power

--- DDA/Ergometer/subscripts/force_and_power.py
+++ DDA/Ergometer/subscripts/force_and_power.py
@@ -31,7 +31,18 @@ def calculate_force(data, p_cal_factor, p_cal_offset):
     return force
 
 
+def calculate_power(omega, force, r1, r2, d):
+    omega_2 = omega[0] * r1 / r2
+    torque = force[0] * d
+    power = torque * omega_2
+    return power
+
+
 def analyze_student(data_dir, cal_factor, cal_offset, student_number):
+    r1 = 200e-3 # m
+    r2 = 63e-3 # m
+    d = 145e-3 # m
+
     data = []
     for i in range(4):
         data_set = np.loadtxt(f'{data_dir}/{student_number}Measurement{i + 1}.csv', skiprows=4, delimiter='\t', unpack=True)
@@ -41,19 +52,26 @@ def analyze_student(data_dir, cal_factor, cal_offset, student_number):
 
     angular_vel = []
     force = []
+    power = []
     for data_set in data:
         ang_vel = calculate_angular_velocity(data_set)
+        angular_vel.append(ang_vel)
         f = calculate_force(data_set, cal_factor, cal_offset)
         f = f, 0.01 * f
         force.append(f)
+        pw = calculate_power(ang_vel, f, r1, r2, d)
+        pw = pw, 0.01 * pw
+        power.append(pw)
         w_r, dw_r = sci_round(ang_vel[0], ang_vel[1])
         f_r, df_r = sci_round(f[0], f[1])
+        p_r, dp_r = sci_round(pw[0], pw[1])
         print(f'Angular velocity: {w_r} +- {dw_r}')
         print(f'Force: {f_r} +- {df_r}')
-        angular_vel.append(ang_vel)
+        print(f'Power: {p_r} +- {dp_r}')
 
     angular_velocity = np.array(angular_vel).transpose()
     force = np.array(force).transpose()
+    power = np.array(power).transpose()
 
     # Plotting
     fig, ax = plt.subplots()
@@ -63,5 +81,9 @@ def analyze_student(data_dir, cal_factor, cal_offset, student_number):
     fig, ax = plt.subplots()
     for data_set in data:
         ax.scatter(angular_velocity[0], force[0])
+
+    fig, ax = plt.subplots()
+    for data_set in data:
+        ax.scatter(angular_velocity[0], power[0])
 
     plt.show()
