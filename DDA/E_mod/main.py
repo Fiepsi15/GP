d769428d9983caf70e45f2e06eb03b0230130b70
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-07-22 20:05:30 +0200

    first version

---
+++ DDA/E_mod/main.py
@@ -0,0 +1,43 @@
+import numpy as np
+from DDA.E_mod.subscripts import dynamisch as dyn
+
+data_dir_dyn = 'DDA/E_mod/daten/dynamisch'
+data_dir_stat = 'DDA/E_mod/daten/statisch'
+Datenbank = {'Kupfer': {'l_ges': 319.5e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 27.17e-3},
+             'Stahl_dünn': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 0.55e-3, 'm': 26.59e-3},
+             'Stahl_dick': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 1e-3, 'm': 46.84e-3},
+             'Alu_dünn': {'l_ges': 302e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 8.93e-3},
+             'Alu_dick': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 2e-3, 'm': 32.16e-3}}
+Datenbank_statisch = {'Kupfer': {'l_ges': 200e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 27.24e-3},
+             'Stahl_dünn': {'l_ges': 200e-3, 'b': 20e-3, 'd': 0.55e-3, 'm': 26.74e-3},
+             'Stahl_dick': {'l_ges': 200e-3, 'b': 20e-3, 'd': 1e-3, 'm': 49.13e-3},
+             'Alu_dünn': {'l_ges': 200e-3, 'b': 20e-3, 'd': 1e-3, 'm': 15.61e-3},
+             'Alu_dick': {'l_ges': 200e-3, 'b': 20e-3, 'd': 2e-3, 'm': 32.08e-3}}
+
+
+def run(staerke, metall, einspannlaenge):
+    mess_daten = np.zeros(0)
+    if staerke == 'dünn':
+        mess_daten = np.loadtxt(f'{data_dir_dyn}/Duenner-{metall}-{einspannlaenge}.txt', skiprows=4, unpack=True, delimiter='\t')
+    elif staerke == 'dick':
+        mess_daten = np.loadtxt(f'{data_dir_dyn}/Dicker-{metall}-{einspannlaenge}.txt', skiprows=4, unpack=True, delimiter='\t')
+    else:
+        mess_daten = np.loadtxt(f'{data_dir_dyn}/{metall}-{einspannlaenge}.txt', skiprows=4, unpack=True, delimiter='\t')
+
+    parameter = Datenbank
+    if staerke != '':
+        parameter = Datenbank[f'{metall}_{staerke}']
+    else:
+        parameter = Datenbank[metall]
+    L = einspannlaenge / 1e3
+
+    dyn.test(mess_daten[1], masse=parameter['m'], L=L, b=parameter['b'], d=parameter['d'], l_ges=parameter['l_ges'])
+
+
+run('', 'Kupfer', 191)
+
+#test_daten = np.loadtxt(f'{data_dir_dyn}/Duenner-Alu-232.txt', skiprows=4, unpack=True, delimiter='\t')
+#
+#alu = Datenbank['Alu_dünn']
+#L = 232e-3
+#dyn.test(test_daten[1], masse=alu['m'], L=L, b=alu['b'], d=alu['d'], l_ges=alu['l_ges'])
---
+++ DDA/E_mod/subscripts/dynamisch.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize, signal
+
+def Traegheitsmoment(b, d):
+    return b * d ** 3 / 12
+
+
+def get_c(n):
+    if n == 1:
+        return 0.59686
+    if n == 2:
+        return 1.49418
+    if n == 3:
+        return 2.50025
+    return 3.49999
+
+
+def E_mod(n, L, mu, I, omega):
+    c = get_c(n)
+    beta = c * np.pi / L
+    E = mu * omega ** 2 / (I * beta ** 4)
+    return E
+
+
+def test(daten:np.ndarray, masse, L, b, d, l_ges):
+    I = Traegheitsmoment(b, d)
+    mu = masse / l_ges
+
+    sampling_rate = 100
+    shift = np.mean(daten)
+    daten = daten - shift
+
+    f, Pxx_den = signal.welch(daten, fs=sampling_rate, nperseg=1024)
+    plt.semilogy(f, Pxx_den)
+    plt.ylim([0.5e-4, 1])
+    plt.xlim([-1, 30])
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.grid()
+
+    plt.show()
+    peak_pos, _ = signal.find_peaks(Pxx_den, prominence=0.001)
+    omega = f[peak_pos][0]
+    print(omega)
+
+    print(E_mod(1, L, mu, I, omega) / 1e9)
+    print(E_mod(1, L, mu, I, 50) / 1e9)
