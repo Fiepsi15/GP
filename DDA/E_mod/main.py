d6289d00a7d8e1b720f39e199d750e574f199ac1
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-07-23 17:33:12 +0200

    refactoring and sampling rate correction

--- DDA/E_mod/main.py
+++ DDA/E_mod/main.py
@@ -1,13 +1,8 @@
 import numpy as np
-from DDA.E_mod.subscripts import dynamisch as dyn
+from DDA.E_mod.subscripts.dynamisch import alu, stahl, kupfer
 
 data_dir_dyn = 'DDA/E_mod/daten/dynamisch'
 data_dir_stat = 'DDA/E_mod/daten/statisch'
-Datenbank = {'Kupfer': {'l_ges': 319.5e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 27.17e-3},
-             'Stahl_dünn': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 0.55e-3, 'm': 26.59e-3},
-             'Stahl_dick': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 1e-3, 'm': 46.84e-3},
-             'Alu_dünn': {'l_ges': 302e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 8.93e-3},
-             'Alu_dick': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 2e-3, 'm': 32.16e-3}}
 Datenbank_statisch = {'Kupfer': {'l_ges': 200e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 27.24e-3},
              'Stahl_dünn': {'l_ges': 200e-3, 'b': 20e-3, 'd': 0.55e-3, 'm': 26.74e-3},
              'Stahl_dick': {'l_ges': 200e-3, 'b': 20e-3, 'd': 1e-3, 'm': 49.13e-3},
@@ -15,29 +10,17 @@ Datenbank_statisch = {'Kupfer': {'l_ges': 200e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 
              'Alu_dick': {'l_ges': 200e-3, 'b': 20e-3, 'd': 2e-3, 'm': 32.08e-3}}
 
 
-def run(staerke, metall, einspannlaenge):
-    mess_daten = np.zeros(0)
-    if staerke == 'dünn':
-        mess_daten = np.loadtxt(f'{data_dir_dyn}/Duenner-{metall}-{einspannlaenge}.txt', skiprows=4, unpack=True, delimiter='\t')
-    elif staerke == 'dick':
-        mess_daten = np.loadtxt(f'{data_dir_dyn}/Dicker-{metall}-{einspannlaenge}.txt', skiprows=4, unpack=True, delimiter='\t')
-    else:
-        mess_daten = np.loadtxt(f'{data_dir_dyn}/{metall}-{einspannlaenge}.txt', skiprows=4, unpack=True, delimiter='\t')
-
-    parameter = Datenbank
-    if staerke != '':
-        parameter = Datenbank[f'{metall}_{staerke}']
-    else:
-        parameter = Datenbank[metall]
-    L = einspannlaenge / 1e3
-
-    dyn.test(mess_daten[1], masse=parameter['m'], L=L, b=parameter['b'], d=parameter['d'], l_ges=parameter['l_ges'])
 
 
-run('dünn', 'Alu', 268)
+print('Alu')
+strk_lng = [[['dünn', 268], ['dünn', 232]], [['dick', 260], ['dick', 233]]]
+alu(strk_lng, data_dir_dyn)
 
-#test_daten = np.loadtxt(f'{data_dir_dyn}/Duenner-Alu-232.txt', skiprows=4, unpack=True, delimiter='\t')
-#
-#alu = Datenbank['Alu_dünn']
-#L = 232e-3
-#dyn.test(test_daten[1], masse=alu['m'], L=L, b=alu['b'], d=alu['d'], l_ges=alu['l_ges'])
+print('\nStahl')
+strk_lng = [[['dünn', 255], ['dünn', 199], ['dünn', 151]], [['dick', 267], ['dick', 239], ['dick', 208]]]
+stahl(strk_lng, data_dir_dyn)
+
+print('\nKupfer')
+laengen = [255, 191, 133]
+kupfer(laengen, data_dir_dyn)
+
--- DDA/E_mod/subscripts/dynamisch.py
+++ DDA/E_mod/subscripts/dynamisch.py
@@ -2,6 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize, signal
 
+Datenbank = {'Kupfer': {'l_ges': 319.5e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 27.17e-3},
+             'Stahl_dünn': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 0.55e-3, 'm': 26.59e-3},
+             'Stahl_dick': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 1e-3, 'm': 46.84e-3},
+             'Alu_dünn': {'l_ges': 302e-3, 'b': 20e-3, 'd': 0.5e-3, 'm': 8.93e-3},
+             'Alu_dick': {'l_ges': 300.5e-3, 'b': 20e-3, 'd': 2e-3, 'm': 32.16e-3}}
+
+
+def find_sampling_rate(file):
+    with open(file, 'r') as f:
+        f.readline()
+        f.readline()
+        line = f.readline()
+        words = []
+        temp = ''
+        for c in line:
+            if c == ' ':
+                words.append(temp)
+                temp = ''
+                continue
+            temp += c
+        rate = words[-1]
+    return int(rate)
+
+
 def Traegheitsmoment(b, d):
     return b * d ** 3 / 12
 
@@ -23,25 +47,96 @@ def E_mod(n, L, mu, I, omega):
     return E
 
 
-def test(daten:np.ndarray, masse, L, b, d, l_ges):
+def test(daten: np.ndarray, masse, L_einsp, b, d, l_ges, sampling_rate):
     I = Traegheitsmoment(b, d)
     mu = masse / l_ges
 
-    sampling_rate = 100
     shift = np.mean(daten)
     daten = daten - shift
 
     f, Pxx_den = signal.welch(daten, fs=sampling_rate, nperseg=1024)
     plt.semilogy(f, Pxx_den)
     plt.ylim([0.5e-4, 1])
-    plt.xlim([-1, 30])
+    plt.xlim([-1, 50])
     plt.xlabel('frequency [Hz]')
     plt.ylabel('PSD [V**2/Hz]')
     plt.grid()
 
-    plt.show()
     peak_pos, _ = signal.find_peaks(Pxx_den, prominence=0.0001)
     omega = f[peak_pos[-1]] * 2 * np.pi
-    print(omega)
+    # print(f'omega = {omega}')
 
-    print(E_mod(1, L, mu, I, omega) / 1e9)
+    E = E_mod(1, L_einsp, mu, I, omega)
+    return E
+
+
+def run(staerke, metall, einspannlaenge, data_dir):
+    mess_daten = np.zeros(0)
+    fp = ''
+    if staerke == 'dünn':
+        fp = f'{data_dir}/Duenner-{metall}-{einspannlaenge}.txt'
+        mess_daten = np.loadtxt(fp, skiprows=4, unpack=True,
+                                delimiter='\t')
+    elif staerke == 'dick':
+        fp = f'{data_dir}/Dicker-{metall}-{einspannlaenge}.txt'
+        mess_daten = np.loadtxt(fp, skiprows=4, unpack=True,
+                                delimiter='\t')
+    else:
+        fp = f'{data_dir}/{metall}-{einspannlaenge}.txt'
+        mess_daten = np.loadtxt(fp, skiprows=4, unpack=True, delimiter='\t')
+
+    parameter = Datenbank
+    if staerke != '':
+        parameter = Datenbank[f'{metall}_{staerke}']
+    else:
+        parameter = Datenbank[metall]
+
+    L = einspannlaenge / 1e3
+    sampling_rate = find_sampling_rate(fp)
+
+    E = test(mess_daten[1], masse=parameter['m'], L_einsp=L, b=parameter['b'], d=parameter['d'],
+             l_ges=parameter['l_ges'], sampling_rate=sampling_rate)
+
+    plt.title(f'{metall} {staerke} bei  {einspannlaenge} mm')
+    plt.show()
+
+    return E
+
+
+def alu(staerken, data_dir):
+    E_mods = []
+    for staerke in staerken:
+        E_mods_s = []
+        for laenge in staerke:
+            E = run(laenge[0], 'Alu', laenge[1], data_dir)
+            E_mods_s.append(E)
+        E_mods.append(E_mods_s)
+    E_mods = np.array(E_mods)
+
+    print(E_mods / 1e9)
+    return
+
+
+def stahl(staerken, data_dir):
+    E_mods = []
+    for staerke in staerken:
+        E_mods_s = []
+        for laenge in staerke:
+            E = run(laenge[0], 'Stahl', laenge[1], data_dir)
+            E_mods_s.append(E)
+        E_mods.append(E_mods_s)
+    E_mods = np.array(E_mods)
+
+    print(E_mods / 1e9)
+    return
+
+
+def kupfer(laengen, data_dir):
+    E_mods = []
+    for laenge in laengen:
+        E = run('', 'Kupfer', laenge, data_dir)
+        E_mods.append(E)
+    E_mods = np.array(E_mods)
+
+    print(E_mods / 1e9)
+    return
