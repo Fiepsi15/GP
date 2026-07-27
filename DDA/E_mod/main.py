273e42df16fe861ae076928384acd2f276bb3824
Sebastian Eisenbarth <sebastian-eisenbarth@gmx.de>
2026-07-27 16:45:48 +0200

    misc

--- DDA/E_mod/main.py
+++ DDA/E_mod/main.py
@@ -6,17 +6,17 @@ data_dir_dyn = 'DDA/E_mod/daten/dynamisch'
 data_dir_stat = 'DDA/E_mod/daten/statisch'
 
 
-#print('Alu')
-#strk_lng = [[['dünn', 268], ['dünn', 232]], [['dick', 260], ['dick', 233]]]
-#alu(strk_lng, data_dir_dyn)
-#
-#print('\nStahl')
-#strk_lng = [[['dünn', 255], ['dünn', 199], ['dünn', 151]], [['dick', 267], ['dick', 239], ['dick', 208]]]
-#stahl(strk_lng, data_dir_dyn)
-#
-#print('\nKupfer')
-#laengen = [255, 191, 133]
-#kupfer(laengen, data_dir_dyn)
+print('Alu')
+strk_lng = [[['dünn', 268], ['dünn', 232]], [['dick', 260], ['dick', 233]]]
+dynamisch.alu(strk_lng, data_dir_dyn)
+
+print('\nStahl')
+strk_lng = [[['dünn', 255], ['dünn', 199], ['dünn', 151]], [['dick', 267], ['dick', 239], ['dick', 208]]]
+dynamisch.stahl(strk_lng, data_dir_dyn)
+
+print('\nKupfer')
+laengen = [255, 191, 133]
+dynamisch.kupfer(laengen, data_dir_dyn)
 
 strength_list = [1, 2] # mm
 statisch.analyze(data_dir_stat, 'Alu', strength_list)
